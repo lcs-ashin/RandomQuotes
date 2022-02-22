18c29443fd8229a275df287150793e2d7ad3e39e
lcs-ashin <ashin@lcs.on.ca>
2022-02-22 09:54:37 -0500

    Initial Commit

---
+++ RandomQuotes/RandomQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuotesApp.swift
+//  RandomQuotes
+//
+//  Created by Eunbi Shin on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
