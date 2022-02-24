7f667da4a06e5053ab4eebe63146b9924282b051
lcs-ashin <ashin@lcs.on.ca>
2022-02-23 22:14:23 -0500

    Created a model page called RandomQuote

---
+++ RandomQuotes/Model/RandomQuote.swift
@@ -0,0 +1,16 @@
+//
+//  RandomQuote.swift
+//  RandomQuotes
+//
+//  Created by Eunbi Shin on 2022-02-23.
+//
+
+import Foundation
+
+struct RandomQuote: Decodable {
+    let quoteText: String
+    let quoteAuthor: String
+    let senderName: String
+    let senderLink: String
+    let quoteLink: String 
+}
